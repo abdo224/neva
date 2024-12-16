20f8c920170e99f7d46d6e743fec8cd00fb8a1f0
Abdeldjaoued BESSABIS <73487494+abdo224@users.noreply.github.com>
2024-12-16 14:38:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bienvenue sur le serveur TCP/HTTP avec Azure App Service !"
+    return "Bienvenue sur le serveur TCP/HTTP d'essai !"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
